919bf6ca1bae1e87fe6f2fcbfdc5b16e468e9740
Krishnath12 <krishnathjadhav@gmail.com>
2023-12-10 20:04:05 +0530

    minor change again

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -8,7 +8,7 @@ const Counter=()=>{
        <div>
            Krishnath Jadhav, Kodani See Chimata <div>Counter:{count}</div>
            <div><button onClick={()=>{setCount(count+1)}}>Increase Count</button></div>
-           Checking for file structure 
+           Checking for file structure again
         </div>
 
     )
